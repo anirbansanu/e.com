0915d774c3ab084efee159e612b36a9680e496b8
anirban <anirbanmukherjee602@gmail.com>
2024-07-06 17:25:08 +0530

    auth name change to username

--- resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -20,10 +20,10 @@
         @if(config('adminlte.usermenu_image'))
             <img src="{{ Auth::user()->adminlte_image() }}"
                  class="user-image img-circle elevation-2"
-                 alt="{{ Auth::user()->name }}">
+                 alt="{{ Auth::user()->username }}">
         @endif
         <span @if(config('adminlte.usermenu_image')) class="d-none d-md-inline" @endif>
-            {{ Auth::user()->name }}
+            {{ Auth::user()->username }}
         </span>
     </a>
 
