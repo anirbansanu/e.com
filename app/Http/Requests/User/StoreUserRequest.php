14415dc53c4392f1761882e40fbc34d4074b0b5b
anirban <anirbanmukherjee602@gmail.com>
2024-07-06 09:28:52 +0530

    add request files

---
+++ app/Http/Requests/User/StoreUserRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'roles' => 'required|array',
+            'roles.*' => 'required|string|exists:roles,name',
+        ];
+    }
+}
