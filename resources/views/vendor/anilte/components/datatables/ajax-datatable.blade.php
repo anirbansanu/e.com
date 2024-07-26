64078ae990901f91aaed904661761bc0201caa66
anirban <anirbanmukherjee602@gmail.com>
2024-07-26 12:36:20 +0530

    some ui changes

--- resources/views/vendor/anilte/components/datatables/ajax-datatable.blade.php
+++ resources/views/vendor/anilte/components/datatables/ajax-datatable.blade.php
@@ -1,9 +1,9 @@
 {{-- Path = resources/views/vendor/anilte/components/datatables/ajax-datatable.blade.php --}}
 <div class="" id="table-container">
     <div class="d-flex flex-row align-items-center justify-content-between">
-        <div class="form-group d-flex flex-row">
-            <label for="show-entries">Show </label>
-            <select id="show-entries" class="form-control select2-single mx-1">
+        <div class="form-group d-flex flex-row align-items-center">
+            <label for="show-entries" class="text-primary m-0 mr-1">Show</label>
+            <select id="show-entries" class="custom-select w-auto form-control-border bg-light">
                 <option value="5">5</option>
                 <option value="10" selected>10</option>
                 <option value="20">20</option>
@@ -12,7 +12,17 @@
 
         </div>
         <div class="form-group">
-            <input type="text" id="search" class="form-control" placeholder="Search...">
+            <div class="input-group input-group-sm" style="width: 250px;">
+                <input type="text" name="search" id="search" class="form-control float-right" placeholder="Search by Name, Description" value="{{ $search??"" }}">
+                <input type="hidden" class="d-none" name="entries" value="{{ $entries??"" }}">
+                <input type="hidden" class="d-none" name="sort_by" value="{{ $sort_by??"" }}">
+                <input type="hidden" class="d-none" name="sort_order" value="{{ $sort_order??"" }}">
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
     <table class="table table-hover text-nowrap border">
@@ -36,7 +46,7 @@
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
-        const dataTable = new CustomDataTable('table-container', '{{ $fetchUrl }}', {!! json_encode($columns) !!}, `{!! $actionButtons !!}`, {{ $pageSize }});
+        const ajaxDataTable = new CustomDataTable('table-container', '{{ $fetchUrl }}', {!! json_encode($columns) !!}, `{!! $actionButtons !!}`, {{ $pageSize }});
     });
 
 </script>
